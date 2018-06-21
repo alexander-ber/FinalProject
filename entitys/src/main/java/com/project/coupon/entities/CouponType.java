0e4651bc17f94235e600166b5716ac3f21ce873a
Alex <alexander.ba@gmail.com>
2018-06-21 21:32:28 +0300

    add Enum converter

--- entitys/src/main/java/com/project/coupon/entities/CouponType.java
+++ entitys/src/main/java/com/project/coupon/entities/CouponType.java
@@ -1,11 +1,23 @@
 package com.project.coupon.entities;
 
 public enum CouponType {
-	RESTURANS,
-	ELECTRICITY,
-	FOOD,
-	HEALTH,
-	SPORTS,
-	CAMPING,
-	TRAVELLING
+	RESTURANS("REST"),
+	ELECTRICITY("ELEC"),
+	FOOD("FOOD"),
+	HEALTH("HEALTH"),
+	SPORTS("SPORTS"),
+	CAMPING("CAMPING"),
+	TRAVELLING("TRAVELLING");
+	
+	private final String code;
+	
+	
+	CouponType(String code){
+		this.code = code;
+	}
+	
+	public String getCode() {
+		return code;
+	}
+	
 }
--- entitys/src/main/java/com/project/coupon/entities/CouponTypeConverter.java
+++ entitys/src/main/java/com/project/coupon/entities/CouponTypeConverter.java
@@ -1,5 +1,28 @@
 package com.project.coupon.entities;
 
-public class CouponTypeConverter {
+import java.util.Iterator;
+
+import javax.persistence.AttributeConverter;
+import javax.persistence.Converter;
+
+@Converter
+public class CouponTypeConverter implements AttributeConverter<CouponType, String> {
+
+	public String convertToDatabaseColumn(CouponType type) {
+		return type.getCode();
+	}
+
+	public CouponType convertToEntityAttribute(String dbData) {
+		final CouponType[] values = CouponType.values();
+		
+		for(CouponType type : values) {
+			if(type.getCode().equals(dbData)) {
+				return type;
+			}
+		}
+
+		return null;
+	}
+	
 	
 }
