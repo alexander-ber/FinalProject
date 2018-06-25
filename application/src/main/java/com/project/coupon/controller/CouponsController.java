8d5da49c5733f8da72fd4f2fee814213dec30810
Alex <alexander.ba@gmail.com>
2018-06-25 19:58:20 +0300

    .

--- application/src/main/java/com/project/coupon/controller/CouponsController.java
+++ application/src/main/java/com/project/coupon/controller/CouponsController.java
@@ -5,6 +5,8 @@ import java.util.Set;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.project.coupon.entities.Company;
@@ -17,9 +19,14 @@ public class CouponsController {
 	private AdminFacade AdminFacadeDAO;	
 
     
-    @GetMapping("/get-all-companies")
-    public List<Company> generateCoupons() {
+    @GetMapping("/admin/get-all-companies")
+    public List<Company> getAllCompanies() {
         return AdminFacadeDAO.getAllCompanies();
-    }	
+    }
+    
+    @PostMapping("/admin/add-company")
+    public void generateCoupons(@RequestBody Company c) {
+         AdminFacadeDAO.createCompany(c);
+    }
     
 }
